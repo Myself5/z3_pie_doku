0fc662816b244f6d53663f120fde8de93c12bf6b
Christian Oder <myself5@carbonrom.org>
2019-01-12 21:49:02 +0100

    z3_doku: Finish up content

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
+# clean auxilary files
 git clean -fxd
+pdflatex seminar.tex
+# Needs to run twice to allow usage of citations in glossaries
 pdflatex seminar.tex
 bibtex seminar
 pdflatex seminar.tex
+# run twice for index lists and glossaries
 pdflatex seminar.tex
-xdg-open seminar.pdf
